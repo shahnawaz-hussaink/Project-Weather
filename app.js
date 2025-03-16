bb38c3a8a8ce52b0d77eb2e4812fe4a7475e148a
Shahnawaz Hussain <shahnawaz.hussain96508@gmail.com>
2025-03-16 22:20:49 +0530

    Update 2.0

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// let h1 = document.querySelector("h1");
+// let h6 = document.querySelector("h6");
+// let h3 = document.querySelector("h3");
+// let p = document.querySelector("p");
+
+// let url = "https://api.openweathermap.org/data/2.5/weather?q=Pune,in&appid=be84cad7ad901cc892741d4a2c42f68b&units=metric"
+// fetch(url)
+//     .then(Response => Response.json())
+//     .then( (data)=>{
+//         h1.innerText = data.name;
+//         h3.innerText = data.name + ", IND";
+//         h6.innerText = data.main.temp + "°C";
+//         p.innerText = data.weather[0].description;
+
+//     }) ;
+    
+    
