456b662f9844746e67236de7ac7e632b797ebd67
Shahnawaz Hussain <shahnawaz.hussain96508@gmail.com>
2025-03-17 12:52:51 +0530

    Update 4.0.1

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+let navCityName = document.getElementsByClassName("nav-city-name");
+let temper = document.getElementById("temp-on-time");
+let discrip = document.getElementById("description");
 
-// let h1 = document.querySelector("h1");
-// let h6 = document.querySelector("h6");
-// let h3 = document.querySelector("h3");
-// let p = document.querySelector("p");
+let city = "New Delhi";
+let btn = document.querySelector("button");
+btn.addEventListener("click",()=>{
+    let cityName = document.getElementById("city-name").value;
+    city = cityName;
 
-// let url = "https://api.openweathermap.org/data/2.5/weather?q=Pune,in&appid=be84cad7ad901cc892741d4a2c42f68b&units=metric"
-// fetch(url)
-//     .then(Response => Response.json())
-//     .then( (data)=>{
-//         h1.innerText = data.name;
-//         h3.innerText = data.name + ", IND";
-//         h6.innerText = data.main.temp + "°C";
-//         p.innerText = data.weather[0].description;
-
-//     }) ;
+    cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
     
-    
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},in&appid=be84cad7ad901cc892741d4a2c42f68b&units=metric`
+    fetch(url)
+        .then(Response => Response.json())
+        .then( (data)=>{
+            console.log(data.name);
+            for (let i = 0; i < navCityName.length; i++) {
+                navCityName[i].innerText = cityName + ", IND";
+            }
+            temper.innerText = data.main.temp + "°C";
+            discrip.innerText = data.weather[0].description;
+
+                });
+})  
